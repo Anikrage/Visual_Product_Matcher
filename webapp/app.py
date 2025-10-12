a6188a2d94c26ba95070467b1b96fb9ad2b1f45d
anikrage <aneek_halder@yahoo.in>
2025-10-12 20:15:55 +0530

    ready for deployment

--- webapp/app.py
+++ webapp/app.py
@@ -176,4 +176,5 @@ def load_more():
     })
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))  
+
